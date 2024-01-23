93007c6ea3c6abb01fe73688fb4bbe351f0de086
bella <isakawaii365@gmail.com>
2024-01-23 21:49:08 +0000

    changes added to function to persist events between refreshes

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,6 +31,8 @@ for (let i = 9; i < 18; i++) {
 
     // Save the event in local storage when the save button is clicked in that timeblock.✅
 
+// Persist events between refreshes of a page.
+
 
 $(".saveBtn").on("click", function(event) {
     event.preventDefault();
@@ -44,12 +46,24 @@ $(".saveBtn").on("click", function(event) {
     renderEvent();
     }
     
+    function renderEvent() {
+        // var userEvent = localStorage.getItem("event-");
+        // return;
+        // console.log(renderEvent);
+        // console.log(userEvent);
+        $("#event-9").text(localStorage.getItem("event-3"));
+    $("#event-10").text(localStorage.getItem("event-4"));
+    $("#event-11").text(localStorage.getItem("event-11"));
+    $("#event-12").text(localStorage.getItem("event-12"));
+    $("#event-13").text(localStorage.getItem("event-13"));
+    $("#event-14").text(localStorage.getItem("event-14"));
+    $("#event-15").text(localStorage.getItem("event-15"));
+    $("#event-16").text(localStorage.getItem("event-16"));
+    $("#event-17").text(localStorage.getItem("event-17"));
 
+    }
 
 // Persist events between refreshes of a page.
 
-function renderEvent() {
-    var userEvent = localStorage.getItem("event-");
-    return;
-    // console.log(renderEvent);
-}
+
+
