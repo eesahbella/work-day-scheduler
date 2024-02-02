012139f249f0f07bfc091f8a508b48bd7a6fa571
bella <isakawaii365@gmail.com>
2024-02-02 23:09:04 +0000

    adds local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,51 +9,51 @@ $("#currentDay").text(today.format("dddd, D MMMM YYYY"));
 var timeblockContainer = $("#timeblock");
 
 for (let i = 9; i < 18; i++) {
-    var form = $("<form>");
-    var hour = $("<div>").addClass("hour col-1 text-center").text(dayjs().hour(i).format("hA"));
-    var eventTextArea = $("<textarea>").addClass("event col-10").attr("id", "event-" + i);
-    var saveButton = $("<button>").addClass("saveBtn col-1").text("Save");
-    
+  var form = $("<form>");
+  var hour = $("<div>")
+    .addClass("hour col-1 text-center")
+    .text(dayjs().hour(i).format("hA"));
+  var eventTextArea = $("<textarea>")
+    .addClass("event col-10")
+    .attr("id", "event-" + i);
+  var saveButton = $("<button>").addClass("saveBtn col-1").text("Save");
 
-    form.append(hour, eventTextArea, saveButton);
-    timeblockContainer.append(form);
+  form.append(hour, eventTextArea, saveButton);
+  timeblockContainer.append(form);
 
-    var currentTime = dayjs().hour();
-    
-    if (i === currentTime) {
-        form.addClass("present");
-    } else if (i < currentTime) {
-        form.addClass("past");
-    } else {
-        form.addClass("future");
-    }
+  var currentTime = dayjs().hour();
 
+  if (i === currentTime) {
+    form.addClass("present");
+  } else if (i < currentTime) {
+    form.addClass("past");
+  } else {
+    form.addClass("future");
+  }
 
-    // Save the event in local storage when the save button is clicked in that timeblock.✅
+  // Save the event in local storage when the save button is clicked in that timeblock.✅
 
-$(".saveBtn").on("click", function(event) {
+  $(".saveBtn").on("click", function (event) {
     event.preventDefault();
-        
+
     var currentIndex = i; // Get the index of the for loop
     var userEvent = $(event.target).siblings(".event").val();
     // console.log(userEvent);
-        
+
     localStorage.setItem("event-" + currentIndex, userEvent);
     renderEvent();
-    });
-    
-    }
-    
+  });
+}
 
+// Persist events between refreshes of a page.
+function renderEvent() {
+  for (let i = 9; i < 18; i++) {
+    $("#event-" + currentIndex).text(
+      localStorage.getItem("event-" + currentIndex)
+    );
+  }
+}
 
-    // Persist events between refreshes of a page.
-    function renderEvent() {
-        for (let i = 9; i < 18; i++) {
-            $("#event-" + currentIndex).text(localStorage.getItem("event-" + currentIndex));
-        }
-        }
-
-
-
-
-
+for (let i = 9; i < 18; i++) {
+    $("form #event-"+ i).val(localStorage.getItem("event-" + i))
+}
